71bb544c14f583d42d48412fb194e2d747e79e12
Tanjila13 <136145710+Tanjila13@users.noreply.github.com>
2023-07-18 00:49:19 +0600

    Add files via upload

--- Python/datetime.py
+++ Python/datetime.py
@@ -1,9 +1,10 @@
+# # import datetime
+
+# # date = datetime.datetime.now()
+# # print(date)
+
 # import datetime
+# x = datetime.datetime.now()
+# print(x.year)
+# print(x.strftime("%A"))
 
-# date = datetime.datetime.now()
-# print(date)
-
-import datetime
-x = datetime.datetime.now()
-print(x.year)
-print(x.strftime("%A"))
